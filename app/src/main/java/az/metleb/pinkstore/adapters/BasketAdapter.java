91fa25dc1d94c66d291bde3b166c12238b96f115
Metleb1996 <metlebrustemov1996@gmail.com>
2023-11-11 21:31:05 +0400

    basket p2

--- app/src/main/java/az/metleb/pinkstore/adapters/BasketAdapter.java
+++ app/src/main/java/az/metleb/pinkstore/adapters/BasketAdapter.java
@@ -1,12 +1,19 @@
 package az.metleb.pinkstore.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
+import az.metleb.pinkstore.R;
 import az.metleb.pinkstore.models.ProductModel;
 
 public class BasketAdapter extends ArrayAdapter<ProductModel> {
@@ -16,5 +23,29 @@ public class BasketAdapter extends ArrayAdapter<ProductModel> {
         super(context, resource, objects);
         this.products = objects;
         this.resource = resource;
+    }
+
+    @Override
+    public int getCount() {
+        return this.products.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View v = convertView;
+
+        if (v == null) {
+            // getting reference to the main layout and initializing
+            LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(this.resource, null);
+        }
+        TextView title = (TextView) v.findViewById(R.id.basket_name);
+        title.setText(this.products.get(position).getText());
+        TextView price = (TextView) v.findViewById(R.id.basket_price);
+        price.setText(String.format("%f",this.products.get(position).getPrice()));
+        ImageView image = (ImageView) v.findViewById(R.id.basket_image);
+        image.setImageResource(this.products.get(position).getImageId());
+        return v;
     }
 }

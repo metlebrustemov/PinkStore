bb6e8b877e3113bbf5513be1e287bf07c27fed09
Memmedova Mesume <memmedovamesume29@gmail.com>
2023-11-18 20:24:05 +0400

    favoritler sehifesi tamamlandi

--- app/src/main/java/az/metleb/pinkstore/adapters/FavoriteAdapter.java
+++ app/src/main/java/az/metleb/pinkstore/adapters/FavoriteAdapter.java
@@ -1,12 +1,60 @@
 package az.metleb.pinkstore.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import az.metleb.pinkstore.R;
+import az.metleb.pinkstore.models.ProductModel;
 
 public class FavoriteAdapter extends ArrayAdapter {
-    public FavoriteAdapter(@NonNull Context context, int resource) {
+    List<ProductModel> favorites;
+    int resuourceID;
+    public FavoriteAdapter(@NonNull Context context, int resource, List<ProductModel> favorites) {
         super(context, resource);
+        this.favorites = favorites;
+        this.resuourceID = resource;
+    }
+
+    @Override
+    public int getCount() {
+        return this.favorites.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View v = convertView;
+
+        if (v == null) {
+            LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(this.resuourceID, null);
+        }
+        ProductModel pm = this.favorites.get(position);
+        TextView textView = (TextView) v.findViewById(R.id.favorit_item_name);
+        textView.setText(pm.getText());
+        TextView textView1 =(TextView) v.findViewById(R.id.favorite_item_price);
+        textView1.setText(String.format("%.2f $",pm.getPrice()));
+        ImageView imageView = (ImageView) v.findViewById(R.id.favorite_item_image);
+        imageView.setImageResource(pm.getImageId());
+        ImageButton imageButton =  (ImageButton) v.findViewById(R.id.buttonFavoritdenSil);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pm.setFavorite(false);
+                notifyDataSetChanged();
+            }
+        });
+        return v;
     }
 }
--- app/src/main/java/az/metleb/pinkstore/fragments/FavoritesFragment.java
+++ app/src/main/java/az/metleb/pinkstore/fragments/FavoritesFragment.java
@@ -7,17 +7,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import az.metleb.pinkstore.R;
+import az.metleb.pinkstore.adapters.FavoriteAdapter;
+import az.metleb.pinkstore.backends.ProductsBackend;
 
 
 public class FavoritesFragment extends Fragment {
-
+ProductsBackend productsBackend = ProductsBackend.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        View view =  inflater.inflate(R.layout.fragment_favorites, container, false);
+        FavoriteAdapter favoriteAdapter = new FavoriteAdapter(getContext(), R.layout.favorite_list_item, productsBackend.getFavorites());
+        ListView listView = (ListView) view.findViewById(R.id.favoritesList);
+        listView.setAdapter(favoriteAdapter);
+        return  view;
     }
 }
